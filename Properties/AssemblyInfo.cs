0f37701a51ef60befd8936fe7ee45bb0bc1b24f1
AngusLiuYi <154724136@qq.com>
2025-10-25 00:33:22 +0800

    更改解决方案名称

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("S20231109")]
+[assembly: AssemblyTitle("ForceControlCalibrationTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("S20231109")]
+[assembly: AssemblyProduct("ForceControlCalibrationTool")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
