75a18f3d3a0f094e0c8c6acafe2c1720a87302a7
AngusLiuYi <154724136@qq.com>
2025-11-03 15:52:14 +0800

    测试其他解决方案中关联AngusTools项目，并在其他解决方案中修改后提交

---
+++ ForceCtrlCailbrationTool_.Net/Form1.cs
@@ -0,0 +1,10 @@
+namespace ForceCtrlCailbrationTool_.Net_x._0_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
